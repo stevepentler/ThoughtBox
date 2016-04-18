cf4213f6e02c6b03648c711d3446e57992bffd08
Steve Pentler <stevepentler@gmail.com>
2016-04-18 12:54:21 -0600

    Successful ajax call on update, but doesn't update database

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -3,4 +3,15 @@ class Api::V1::LinksController < ApplicationController
   def index
     respond_with current_user.links
   end
+
+  def update
+    @link = Link.find(params[:id])
+    respond_with @link.update(idea_params)
+  end
+
+  private
+
+  def idea_params
+    params.require(:link).permit(:title, :url, :viewed)
+  end
 end
