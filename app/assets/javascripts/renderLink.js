3d68223c2221994f9252fc5aa9b655831323d6c9
Steve Pentler <stevepentler@gmail.com>
2016-04-18 11:43:09 -0600

    Adds render link JS using jquery, needs API

---
+++ app/assets/javascripts/renderLink.js
@@ -0,0 +1,8 @@
+function renderLink(link) {
+  $('#links').prepend(
+    `<div class='link center' link-id=${link.id}>
+      <h3 class='title'> ${link.title}</h3>
+      <h3 class='url'> ${link.url}</h3>
+      <h4 class='viewed'> ${link.viewed}</h4>
+    </div>`
+}
