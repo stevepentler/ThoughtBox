ec6c301f7deba126c1460b2a5895518b188a11d6
Steve Pentler <stevepentler@gmail.com>
2016-04-18 10:30:08 -0600

    User signup test

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
-  test 'unauthenticated user can login' do
+  test 'unauthenticated user can sign up' do
     visit root_path
     assert page.has_content?("Sign Up")
     assert page.has_content?("Email address")
+    fill_in "user[email]", with: "stevepentler@gmail.com"
     assert page.has_content?("Password")
+    fill_in "user[password]", with: "GoBadgers"
     assert page.has_content?("Password confirmation")
+    fill_in "user[password_confirmation]", with: "GoBadgers"
+    click_on "Create Account"
+
+    assert_equal links_path, current_path
+
   end
 end
