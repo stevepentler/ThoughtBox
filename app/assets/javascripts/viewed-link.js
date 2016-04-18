1d9d2575f5bdf06794d0060be2dc2e60b0d1a7f2
Steve Pentler <stevepentler@gmail.com>
2016-04-18 14:02:20 -0600

    Trouble editing view state, moving on to search functionality

---
+++ app/assets/javascripts/viewed-link.js
@@ -0,0 +1,37 @@
+function viewedLink(status) {
+  $('#link-index').delegate(status, 'click', function() {
+    let $link = $(this).closest('.link');
+    let previousState = $idea.find('p').text();
+    let newViewState = updatedState(status, previousState);
+    let linkParams = {
+      link: {viewed: newViewState}
+    }
+  })
+  viewStatusCall(link, linkParams, newViewState)
+}
+
+function viewStatusCall(link, linkParams, newViewState) {
+  $.ajax({
+    type: "PUT",
+    url: `/api/v1/links/${link.attr('link-id')}`,
+    data: linkParams,
+    success: function() {
+      console.log("updated views status")
+      link.find('p').innerHTML = newViewState
+    },
+    error: function(error) {
+      console.log(error.responseText);
+    }
+  })
+}
+
+function updatedStatus(status, previousState) {
+  console.log(status)
+  if (status === true) {
+    return `<p>Mark as Unread</p>`
+  } else {
+    return `<p>Mark as Read</p>`
+  }
+};
+
+viewedLink('.viewed')
