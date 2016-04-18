c9ac24b8e7de21aaf43f1e56ca24ca6aa2074365
Steve Pentler <stevepentler@gmail.com>
2016-04-18 16:36:59 -0600

    Life changing commit

--- app/assets/javascripts/filter-links.js
+++ app/assets/javascripts/filter-links.js
@@ -1,17 +1,8 @@
-// function toggleViewed() {
-//   $('#filter-viewed').on('click', function() {
-//     let linksIndex = $('#links-index').children();
-//     let viewStatus = this.innerHTML;
-//     toggleFilter(linksIndex, viewStatus);
-//   })
-// }
-
-// function toggleFilter(linksIndex, viewStatus) {
-//   console.log(viewStatus)
-//   if (viewStatus === "View Unread") {
-//     $(linksIndex).filter(link.viewed === 0)
-//     $('#filter-viewed').innerHTML = "View Read";
-//   })
-// }
+function toggleViewed() {
+  $('#filter-viewed').on('click', function() {
+    let linksIndex = $('#links-index').children();
+    let viewStatus = this.innerHTML;
+  })
+}
 
 
