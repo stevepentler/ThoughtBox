b9b62ea1fc9c2709f907c3ca99b4ca2c71d1a585
Steve Pentler <stevepentler@gmail.com>
2016-04-18 10:22:26 -0600

    Login & Logout functional

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
+
   def new
-    @user = User.new
+    if current_user
+      redirect_to links_path
+    end
+
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
  root to: "users#new"
 
  resources :users
+
+ get '/login', to: 'sessions#new'
+ post '/login', to: 'sessions#create'
+ delete '/logout', to: 'sessions#destroy'
  resources :links
 end
