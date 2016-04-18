61faf4d5ddf2bdd9aa5ee416ae3563f880741d93
Steve Pentler <stevepentler@gmail.com>
2016-04-18 09:58:03 -0600

    Creates account hits links index

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
  root to: "users#new"
 
  resources :users
+ resources :links
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UserLoginTest < ActionDispatch::IntegrationTest
   test 'unauthenticated user can login' do
     visit root_path
-    assert page.has_content?("Create Account")
+    assert page.has_content?("Sign Up")
     assert page.has_content?("Email address")
     assert page.has_content?("Password")
     assert page.has_content?("Password confirmation")
